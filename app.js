0cf5b4cb8b9364cb3cbb936a8b24d33b67b775a2
SUNSOUL085 <101106376+SUNSOUL085@users.noreply.github.com>
2023-01-12 23:11:06 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// import '@fortawesome/fontawesome-free/js/fontawesome';
+
+import './scripts/components/navbar/navbar.js';
+import './scripts/components/jumbotron/jumbotron.js';
+import './scripts/components/category/category.js';
+import './scripts/components/category-list.js';
+import './scripts/components/client/client.js';
+import './scripts/components/partner/partner.js';
+import './scripts/components/aplikasi/aplikasi.js';
+import './scripts/components/footer/footer.js';
+
+
+import main from './scripts/pages/index.js';
+document.addEventListener('DOMContentLoaded', main);
